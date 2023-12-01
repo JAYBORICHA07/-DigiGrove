630865cf6d7fc71c9273c758b779f187fe90dc56
JAYBORICHA <jayboricha707@gmail.com>
2023-12-02 00:01:24 +0530

    Made the maxwidth component and basic page component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en" className='h-full'>
       <body className={cn("relative h-full font-sans antialiased",inter.className)}>
         <main className='relative flex flex-col min-h-screen'>
-          <div>
+          <div className='flex-grow flex-1'>
           {children}
           </div>
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-    <p>hello</p>
-    </>
+    <MaxWidthWrapper>
+      <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
+        <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+          Your market place for high-quality{' '}<span className='text-blue-600'>Digital Assets</span>
+        </h1>
+      </div>
+    </MaxWidthWrapper>
   )
 }
