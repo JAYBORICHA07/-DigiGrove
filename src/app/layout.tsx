47624ca44a74226e820b65501ad7505bb401b4a1
JAYBORICHA <jayboricha707@gmail.com>
2023-12-01 23:46:31 +0530

    Comtinued with the project:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
-import { Main } from 'next/document'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +19,9 @@ export default function RootLayout({
     <html lang="en" className='h-full'>
       <body className={cn("relative h-full font-sans antialiased",inter.className)}>
         <main className='relative flex flex-col min-h-screen'>
+          <div>
           {children}
+          </div>
         </main>
       </body>
     </html>
