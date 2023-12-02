2128f591192fd774056d6548179991bf6d04e574
JAYBORICHA <jayboricha707@gmail.com>
2023-12-02 23:35:29 +0530

    continued with the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import { Button, buttonVariants } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,6 +10,11 @@ export default function Home() {
         <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
           Your market place for high-quality{' '}<span className='text-blue-600'>Digital Assets</span>
         </h1>
+        <p className='mt-6 text-lg max-w-prose text-muted-foreground'>Welcome to DigiGrove. Every asset on our platform is verified by our team to ensure our highest quality standards </p>
+        <div className='flex flex-col sm:flex-row gap-4 mt-6'>
+          <Link href='/products' className={buttonVariants()}>Browse Trending</Link>
+          <Button variant='ghost'>Our quality promice &rarr;</Button>
+        </div>
       </div>
     </MaxWidthWrapper>
   )
