6bcc1e99450dcb95ff2dde9fe7eb4a469330552f
JAYBORICHA <jayboricha707@gmail.com>
2024-02-06 17:06:05 +0530

    Continued with the navbar

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useState } from "react"
+
+export default function NavItems() {
+
+    const [activeIndev, setActiveIndex] = useState<null | number>(null)
+
+  return (
+    <div className="flex gap-4 h-full">
+        
+    </div>
+  )
+}
